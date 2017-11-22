25fe8714e6e89cb4d25d7e419c3fb919520fcd2e
Rahul <rahulrajgupta83@gmail.com>
2017-11-22 17:46:59 +0545

    Improved.

---
+++ includes/header2.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Sugarwish customer bootsrtap 4 beta</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKitEmmi.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKitUma2.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/MyFontsWebfontsKitUma3.css" media="all" />
+    <link href="css/consumer-styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="page-wrapper">
+        <header id="header" class="header2">
+            <div class="fixed-top bg-white">
+                <div class="container">
+                    <div class="row no-gutters">
+                        <div class="col-md-12 col-lg-8 mx-auto">
+                            <div class="row">
+                                <div class="col-lg-11 mx-auto px-md-3 px-lg-2">
+                                    <div class="row align-items-center">
+                                        <div class="col-3 text-center">
+                                            <a href="#">
+                                                <i class="fa fa-arrow-circle-left fa-2x"></i>
+                                            </a>
+                                        </div>
+                                        <div class="col-6">
+                                            <a href="#">
+                                                <img src="img/Sugarwish_Logo.png" class="img-fluid mx-auto d-block" alt="logo image">
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- /header -->
