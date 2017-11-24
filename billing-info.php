f39f0b0197a9076aad93d1d50980e4dd3d9f5230
Rahul <rahulrajgupta83@gmail.com>
2017-11-24 16:32:10 +0545

    designed billing info and payement info and step 2.

---
+++ billing-info.php
@@ -0,0 +1,48 @@
+<?php include('includes/header2.php') ?>
+	<main id="page-content">
+        <div class="container">
+            <div class="row no-gutters my-3 my-md-4">
+                <div class="col-md-12 col-lg-8 mx-auto">
+                    <div class="row">
+                        <div class="col-lg-11 mx-auto px-md-3 px-lg-2">
+                            <div class="">
+                                <div class="">
+                                    <h3 class="cstm-card-header text-center ff-uma-bold mt-4 mb-4 mb-md-4">
+                                        Billing information
+                                    </h3>
+                                    <div class="row">
+                                        <div class="col-md-6 mx-auto">
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="">
+                                                        first name
+                                                    </label>
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">last name</label>
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">phone number</label>
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                                <div class="button-cstm-img mt-2">
+                                                    <button class="outer-img-btn activity" type="submit" id="btn-buyer-step-one">
+                                                        <img src="img/default-btn-background.png" class="img-fluid" alt="img-btn">
+                                                        <span>Continue Checkout</span>
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php include('includes/footer.php') ?>
---
+++ cart.php
@@ -0,0 +1,122 @@
+<?php include('includes/header2.php') ?>
+	<main id="page-content">
+        <div class="container">
+            <div class="row no-gutters cart-page">
+                <div class="col-md-12 col-lg-8 mx-auto">
+                    <div class="row">
+                        <div class="col-lg-11 mx-auto px-md-3 px-lg-2">
+                            <h3 class="cstm-card-header text-center text-dark my-3 my-lg-4">
+                                your cart
+                            </h3>
+                            <div class="card border-0 rounded-0 mb-3 mb-lg-4" style="z-index: 0;">
+                                <div class="card-body">
+                                    <div class="cart-item">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="cart-item-img-sec">
+                                                    <div class="cart-btm-box-shadow position-relative bg-white pb-1 pb-md-3">
+                                                        <div class="cart-top-right-box-shadow2">
+                                                            <div class="heading text-center">
+                                                                <h3 class="title mb-0 ff-uma-bold">
+                                                                    BACK TO SCHOOL
+                                                                </h3>
+                                                                <p class="subtitle mb-1">(Here's to a sweet start)</p>
+                                                            </div>
+                                                            <div class="img-wrap">
+                                                                <img src="http://manage.sugarwish.com/company_bowl/9436_7701_candy-bowls.png" class="img-fluid" alt="candies image">
+                                                            </div>
+                                                            <div class="message">
+                                                                <div>
+                                                                    Dear Linda,<br>
+                                                                    Hope you have a blessed holiday season!<br>
+                                                                    Sweet wishes,<br>
+                                                                    Dana
+                                                                    <img src="https://sugarwish.com/skin/frontend/sugarwish/sugarwish_sender/images/Sugarwish_Logo.png" class="img-fluid w-25 float-right" alt="">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="detail-sec">
+                                                    Duet to Jason B Kiefer <br>
+                                                    Send: 12/31/18 <br>
+                                                    Price: $18.50
+                                                </div>
+                                                <a href="#" class="text-decoration">remove</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="cart-item">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="cart-item-img-sec position-relative bg-white pb-1 pb-md-3">
+                                                    <img src="https://sugarwish.com/media/catalog/product/b/r/brown-birthdaythumbb_1_4.png" class="img-fluid" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="detail-sec">
+                                                    Duet to Jason B Kiefer <br>
+                                                    Send: 12/31/18 <br>
+                                                    Price: $18.50
+                                                </div>
+                                                <a href="#" class="text-decoration">remove</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="totaling-footer">
+                                        <div class="row">
+                                            <dic class="col-md-6 ml-auto">
+                                                <div class="totaling-container ff-uma-bold text-right mt-1 mt-md-2">
+                                                    <div class="sub-total">
+                                                        Order Subtotal:
+                                                        <span class="ml-4">
+                                                            $0.00
+                                                        </span>
+                                                    </div>
+                                                    <div class="sub-total">
+                                                        Discount Applied:
+                                                        <span class="ml-4">
+                                                            $0.00
+                                                        </span>
+                                                    </div>
+                                                    <div class="">
+                                                        Order Total:
+                                                        <span class="ml-4">
+                                                            $0.00
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </dic>
+                                        </div>
+                                        <div class="clearfix my-2">
+                                            <div class="float-left">
+                                                <a id="preview-giftcard" class="btn btn-keep-original btn_gradient  font-weight-bold" href="#">Promo Code</a>
+                                            </div>
+                                            <div class="float-right text-right">
+                                                <a id="preview-giftcard" class="btn btn-keep-original btn_gradient  font-weight-bold" href="#">
+                                                    buy another sugarwish
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <div class="button-cstm-img my-3">
+                                    <button class="outer-img-btn activity" type="submit" id="btn-buyer-step-one">
+                                        <img src="img/default-btn-background.png" class="img-fluid" alt="img-btn">
+                                        <span>proceed to checkout</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php include('includes/footer.php') ?>
