bc3172db14b044f1df02d42de944edec97e38ea9
Rahul <rahulrajgupta83@gmail.com>
2017-11-29 14:36:56 +0545

    Designed modals for step 2.

--- personalize_this_card_step2.php
+++ personalize_this_card_step2.php
@@ -125,7 +125,10 @@
                                     <div class="card-body py-2 py-md-4">
                                         <div class="clearfix">
                                             <div class="float-left">
-                                                <a id="preview-giftcard" class="btn btn-keep-original btn_gradient" href="javascript:void(0);" data-toggle="modal" data-target="#gift-card-preview">preview card</a>
+                                                <!-- <a id="preview-giftcard" class="btn btn-keep-original btn_gradient" href="javascript:void(0);" data-toggle="modal" data-target="#gift-card-preview">preview card</a> -->
+                                                <button id="btn-add-recipient" class="btn btn-keep-original btn_gradient"  data-toggle="modal" data-target=".bd-example-modal-lg">
+                                                    preview card
+                                                </button>
                                             </div>
                                             <div class="float-right text-right">
                                                 <button id="btn-add-recipient" class="btn btn-keep-original btn_gradient">add another recipient</button>
@@ -192,4 +195,104 @@
             </div>
         </div>  
     </main>
+    
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="row">
+            <div class="col-md-12 col-lg-10 mx-auto">
+                <div class="row">
+                    <div class="col-lg-11">
+                        <div class="modal-content preview-card-modal">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span class="text-hide" aria-hidden="true">&times;</span>
+                            </button>
+                            <div class="card-body p-2">
+                                    
+                                <div class="border">
+                                    <div class="p-3">
+                                        <img src="img/Sugarwish_Logo.png" class="img-fluid mx-auto d-block" alt="logo image">
+                                        <h3 class="text-center">Lorem ipsum dolor sit amet.</h3>
+                                        <h3 class="text-center">Lorem ipsum dolor sit amet.</h3>
+                                        <img class="img-fluid col-md-10 mx-auto d-block" src="img/8711_7587_coloradostateu-default.jpg" alt="image">
+                                        <hr>
+                                        <div class="detail-content text-center">
+                                            <h4 class="text-dark mb-3">
+                                                You have been sent a Sugarwish
+                                                <sup style="font-size: 14px ">TM</sup>
+                                                !
+                                            </h4>
+                                            <h4 class="mb-3">
+                                                A Sugarwish is a candy gift that allows <span style="color: #fc4b52; ">you</span> to choose the candy that you want to receive.
+                                            </h4>
+                                            <h4 class="mb-3">
+                                                And best of all, everything, including shipping, has been covered.
+                                            </h4>
+                                            <h4 class="mb-3">
+                                                When you click on the button below, you will be taken to the Sugarwish website to pick your favorite candies--they will arrive lickity-split!
+                                            </h4>
+                                            <div class="mb-3 w-50 mx-auto">
+                                                <a href="javascript:void(0);" class="btn btn-block btn-danger choose-my-candy-btn">choose my candy now!</a>
+                                            </div>
+                                            <h4 class="mb-3">
+                                                (For your reference, your gift code is "XXXX-XXXX-XXXX" but when you click on the button above, your code will transfer automatically.)
+                                            </h4>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer justify-content-center text-white">
+                                        <p class="text-center small">
+                                            If you have any questions please feel free to contact us at:
+                                            <br>
+                                            Sugarwish 4665 E. Warren St, Denver, CO 80222
+                                            <br>
+                                            via email: <span class="text-danger ">info@sugarwish.com</span>
+                                            <br>
+                                            or call us at <a href=" ">720-722-4071</a> Monday -Friday, 8am - 5pm MST.
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+      </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="add-receiver-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content add-receiver-modal">
+            <div class="pt-2 pr-2">
+                <button type="button" class="close btn_gradient" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true"> ✗ </span>
+                </button>
+            </div>
+            <div class="modal-body pt-1">
+                <h3 class="modal-title text-center">Add Recipients From a List</h3>
+                <p class="text-center">To add a list of recipients please copy and paste a list of recipients in the following format (separate lines with returns):
+                    name,email,send date <br>
+                    e.g: <br>
+                    Jane Smith,jane.smith@gmail.com,12/31/2018 <br>
+                        John Smith,john.smith@gmail.com,12/31/2018</p>
+            </div>
+            <div class="card bg-transparent">
+                <div class="card-body">
+                    <textarea id="receiver-list" name="receiver_list" class="form-control" cols="30" rows="10"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-center">
+                <div class="button-cstm-img">
+                    <button class="outer-img-btn activity" type="submit" id="btn-buyer-step-one">
+                        <img src="img/default-btn-background.png" class="img-fluid" alt="img-btn">
+                        <span>Ok</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
 <?php include('includes/footer.php') ?>
